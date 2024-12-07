4fad4e9ac4adb0cb655430f705082c51ad290e43
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-12-07 09:17:52 +0800

    fix ComboBox border RowSpan;

---
+++ src/CodeWF.AvaloniaControls.Demo/ViewModels/VComboBoxDemoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWF.AvaloniaControls.Demo.ViewModels
+{
+    internal class VComboBoxDemoViewModel
+    {
+        public List<string> Numbers { get; } = new List<string>() { "正常", "告警", "异常" };
+    }
+}
