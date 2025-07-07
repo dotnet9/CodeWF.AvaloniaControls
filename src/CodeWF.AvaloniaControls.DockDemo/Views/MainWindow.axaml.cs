6da02fec92b20af40a6e74eabe58f89a70a82e6f
Dotnet9(dotnet9.com) <1012434131@qq.com>
2025-07-07 17:44:17 +0800

    add DockDemo;

---
+++ src/CodeWF.AvaloniaControls.DockDemo/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Ursa.Controls;
+
+namespace CodeWF.AvaloniaControls.DockDemo.Views;
+
+public partial class MainWindow : UrsaWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
