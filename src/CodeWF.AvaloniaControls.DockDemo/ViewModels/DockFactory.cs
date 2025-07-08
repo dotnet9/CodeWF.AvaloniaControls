34ab88a696e9673fcdc6dadedb2a62e57acdc768
Dotnet9(dotnet9.com) <1012434131@qq.com>
2025-07-08 18:23:35 +0800

    remove other;

--- src/CodeWF.AvaloniaControls.DockDemo/ViewModels/DockFactory.cs
+++ src/CodeWF.AvaloniaControls.DockDemo/ViewModels/DockFactory.cs
@@ -1,8 +1,6 @@
 ﻿using CodeWF.AvaloniaControls.DockDemo.Models.Documents;
-using CodeWF.AvaloniaControls.DockDemo.Models.Tools;
 using CodeWF.AvaloniaControls.DockDemo.ViewModels.Docks;
 using CodeWF.AvaloniaControls.DockDemo.ViewModels.Documents;
-using CodeWF.AvaloniaControls.DockDemo.ViewModels.Tools;
 using CodeWF.AvaloniaControls.DockDemo.ViewModels.Views;
 using Dock.Avalonia.Controls;
 using Dock.Model.Controls;
@@ -29,77 +27,16 @@ public class DockFactory : Factory
 
     public override IRootDock CreateLayout()
     {
-        var document1 = new DocumentViewModel { Id = "Document1", Title = "Document1" };
-        var document2 = new DocumentViewModel { Id = "Document2", Title = "Document2" };
+        var document1 = new DocumentViewModel { Id = "Document1", Title = "Document1", CanClose = false };
+        var document2 = new DocumentViewModel { Id = "Document2", Title = "Document2", CanClose = false};
         var document3 = new DocumentViewModel { Id = "Document3", Title = "Document3", CanClose = true };
-        var tool1 = new Tool1ViewModel { Id = "Tool1", Title = "Tool1" };
-        var tool2 = new Tool2ViewModel { Id = "Tool2", Title = "Tool2" };
-        var tool3 = new Tool3ViewModel { Id = "Tool3", Title = "Tool3", CanDrag = false };
-        var tool4 = new Tool4ViewModel { Id = "Tool4", Title = "Tool4", CanDrag = false };
-        var tool5 = new Tool5ViewModel { Id = "Tool5", Title = "Tool5" };
-        var tool6 = new Tool6ViewModel { Id = "Tool6", Title = "Tool6", CanClose = true, CanPin = true };
-        var tool7 = new Tool7ViewModel { Id = "Tool7", Title = "Tool7", CanClose = false, CanPin = false };
-        var tool8 = new Tool8ViewModel { Id = "Tool8", Title = "Tool8", CanClose = false, CanPin = true };
-
-        var leftDock = new ProportionalDock
-        {
-            Proportion = 0.25,
-            Orientation = Orientation.Vertical,
-            ActiveDockable = null,
-            VisibleDockables = CreateList<IDockable>
-            (
-                new ToolDock
-                {
-                    ActiveDockable = tool1,
-                    VisibleDockables = CreateList<IDockable>(tool1, tool2),
-                    Alignment = Alignment.Left,
-                    // CanDrop = false
-                },
-                new ProportionalDockSplitter { CanResize = true },
-                new ToolDock
-                {
-                    ActiveDockable = tool3,
-                    VisibleDockables = CreateList<IDockable>(tool3, tool4),
-                    Alignment = Alignment.Bottom,
-                    CanDrag = false,
-                    CanDrop = false
-                }
-            ),
-            // CanDrop = false
-        };
-
-        var rightDock = new ProportionalDock
-        {
-            Proportion = 0.25,
-            Orientation = Orientation.Vertical,
-            ActiveDockable = null,
-            VisibleDockables = CreateList<IDockable>
-            (
-                new ToolDock
-                {
-                    ActiveDockable = tool5,
-                    VisibleDockables = CreateList<IDockable>(tool5, tool6),
-                    Alignment = Alignment.Top,
-                    GripMode = GripMode.Hidden
-                },
-                new ProportionalDockSplitter(),
-                new ToolDock
-                {
-                    ActiveDockable = tool7,
-                    VisibleDockables = CreateList<IDockable>(tool7, tool8),
-                    Alignment = Alignment.Right,
-                    GripMode = GripMode.AutoHide
-                }
-            ),
-            // CanDrop = false
-        };
 
         var documentDock = new CustomDocumentDock
         {
             IsCollapsable = false,
             ActiveDockable = document1,
             VisibleDockables = CreateList<IDockable>(document1, document2, document3),
-            CanCreateDocument = true,
+            CanCreateDocument = false,
             // CanDrop = false,
             EnableWindowDrag = true,
         };
@@ -109,11 +46,7 @@ public class DockFactory : Factory
             Orientation = Orientation.Horizontal,
             VisibleDockables = CreateList<IDockable>
             (
-                leftDock,
-                new ProportionalDockSplitter(),
-                documentDock,
-                new ProportionalDockSplitter(),
-                rightDock
+                documentDock
             )
         };
 
@@ -162,14 +95,6 @@ public class DockFactory : Factory
             ["Document1"] = () => new DemoDocument(),
             ["Document2"] = () => new DemoDocument(),
             ["Document3"] = () => new DemoDocument(),
-            ["Tool1"] = () => new Tool1(),
-            ["Tool2"] = () => new Tool2(),
-            ["Tool3"] = () => new Tool3(),
-            ["Tool4"] = () => new Tool4(),
-            ["Tool5"] = () => new Tool5(),
-            ["Tool6"] = () => new Tool6(),
-            ["Tool7"] = () => new Tool7(),
-            ["Tool8"] = () => new Tool8(),
             ["Dashboard"] = () => layout,
             ["Home"] = () => _context
         };
