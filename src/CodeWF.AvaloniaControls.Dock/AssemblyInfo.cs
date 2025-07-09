b1304349f474ebdf2c6dec5ce570adac9c0b22c7
Dotnet9(dotnet9.com) <1012434131@qq.com>
2025-07-09 17:19:01 +0800

    add dock theme;

---
+++ src/CodeWF.AvaloniaControls.Dock/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using Avalonia.Metadata;
+
+[assembly: XmlnsPrefix("https://codewf.com", "CodeWF.AvaloniaControls.Dock")]
+[assembly: XmlnsDefinition("https://codewf.com", "CodeWF.AvaloniaControls.Dock")]
---
+++ src/CodeWF.AvaloniaControls.Dock/DockCodeWFTheme.axaml.cs
@@ -0,0 +1,7 @@
+﻿using Avalonia.Styling;
+
+namespace CodeWF.AvaloniaControls.Dock;
+
+public class DockCodeWFTheme : Styles
+{
+}
