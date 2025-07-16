d83f5f10e0fb6aeb8b93d8cbfd6a9ad428af0f9e
Dotnet9(dotnet9.com) <1012434131@qq.com>
2025-07-16 18:43:39 +0800

    update layout;

---
+++ src/CodeWF.AvaloniaControls.DataGridDemo/Pages/SortingView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CodeWF.AvaloniaControls.DataGridDemo.Pages;
+
+public partial class SortingView : UserControl
+{
+    public SortingView()
+    {
+        InitializeComponent();
+    }
+}
