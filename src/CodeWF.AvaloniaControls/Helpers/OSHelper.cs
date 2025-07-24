2e81fd21271aebd393b15c887d8dd6e9eb8ac62e
Dotnet9(dotnet9.com) <1012434131@qq.com>
2025-07-24 20:11:58 +0800

    add OSHelper;

---
+++ src/CodeWF.AvaloniaControls/Helpers/OSHelper.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Controls;
+using System;
+
+namespace CodeWF.AvaloniaControls.Helpers;
+
+public static class OSHelper
+{
+    public static bool IsModernWindowSystem { get; } =
+        !OperatingSystem.IsWindows() || OperatingSystem.IsWindowsVersionAtLeast(6, 2);
+
+    public static ThreadStaticAttribute EnableOSVersionAwareDecorations<T>(this T window) where T : Window
+    {
+        window.SystemDecorations = IsModernWindowSystem ? SystemDecorations.Full : SystemDecorations.None;
+        return new ThreadStaticAttribute();
+    }
+}
