bfa3cdaa425b01e756576ee428f7a81530c1d338
Dotnet9(dotnet9.com) <1012434131@qq.com>
2025-08-13 09:36:41 +0800

    add IsDoubleClickRow;

--- src/CodeWF.AvaloniaControls.DataGrid/DataGridExtension.cs
+++ src/CodeWF.AvaloniaControls.DataGrid/DataGridExtension.cs
@@ -2,11 +2,17 @@
 using Avalonia.Controls;
 using System.ComponentModel;
 using System.Linq;
+using Avalonia.Input;
+using Avalonia.VisualTree;
 
 namespace CodeWF.AvaloniaControls;
 
 public static class DataGridExtension
 {
+    /// <summary>
+    /// 添加排序，点击列有三种状态：未排序、升序、降序。
+    /// </summary>
+    /// <param name="dataGrid"></param>
     public static void AddSorting(this DataGrid dataGrid)
     {
         var view = new DataGridCollectionView(dataGrid.ItemsSource);
@@ -25,5 +31,18 @@ public static class DataGridExtension
             dataGrid.ItemsSource = view;
             view.Refresh();
         };
+    }
+
+    /// <summary>
+    /// 判断是否双击了DataGrid的行
+    /// </summary>
+    /// <param name="e"></param>
+    /// <returns></returns>
+    public static bool IsDoubleClickRow(TappedEventArgs? e)
+    {
+        if (e is null) return true;
+        if (e.Source is not Control control) return false;
+        var row = control.FindAncestorOfType<DataGridRow>();
+        return row is not null;
     }
 }
