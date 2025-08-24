5bf73453dcc35114bb2002bcea759a6a19192777
Dotnet9(dotnet9.com) <1012434131@qq.com>
2025-08-24 17:58:05 +0800

    add control helper;

---
+++ src/CodeWF.AvaloniaControls/Helpers/ControlHelper.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.VisualTree;
+
+namespace CodeWF.AvaloniaControls.Helpers;
+
+public static class ControlHelper
+{
+    public static T? FindAncestor<T>(this Visual visual) where T : Visual
+    {
+        var parent = visual;
+        while (parent != null)
+        {
+            parent = parent.GetVisualParent();
+            if (parent is T typedParent)
+            {
+                return typedParent;
+            }
+        }
+
+        return null;
+    }
+}
