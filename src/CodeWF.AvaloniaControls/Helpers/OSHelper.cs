951f110a57e045700dd29e8228c9a3c0303ca1fd
Dotnet9(dotnet9.com) <1012434131@qq.com>
2025-09-29 14:05:08 +0800

    add win7 resize helper;

--- src/CodeWF.AvaloniaControls/Helpers/OSHelper.cs
+++ src/CodeWF.AvaloniaControls/Helpers/OSHelper.cs
@@ -5,12 +5,32 @@ namespace CodeWF.AvaloniaControls.Helpers;
 
 public static class OSHelper
 {
+    /// <summary>
+    /// 判断当前操作系统是否支持现代窗口装饰
+    /// <value>
+    ///     true: win8+ 或 非Windows系统
+    ///     false: win7及更早版本
+    /// </value>
+    /// </summary>
     public static bool IsModernWindowSystem { get; } =
         !OperatingSystem.IsWindows() || OperatingSystem.IsWindowsVersionAtLeast(6, 2);
 
-    public static ThreadStaticAttribute EnableOSVersionAwareDecorations<T>(this T window) where T : Window
+    /// <summary>
+    /// 根据操作系统版本启用或禁用窗口装饰
+    /// - Windows 8及更高版本或非Windows系统启用窗口装饰
+    /// - Windows 7及更早版本禁用窗口装饰
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="window"></param>
+    /// <returns></returns>
+    public static T EnableOSVersionAwareDecorations<T>(this T window) where T : Window
     {
         window.SystemDecorations = IsModernWindowSystem ? SystemDecorations.Full : SystemDecorations.None;
-        return new ThreadStaticAttribute();
+        if(!IsModernWindowSystem && window.CanResize)
+        {
+            var helper = new WindowResizeHelper(window);
+            helper.EnableResizing();
+        }
+        return window;
     }
 }
