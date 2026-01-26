0985c4ea56f436da255359d114c14de9ae4c92f6
Dotnet9(dotnet9.com) <1012434131@qq.com>
2026-01-26 16:02:35 +0800

    add TreeDataGridExtension;

---
+++ src/CodeWF.AvaloniaControls.DataGrid/TreeDataGridExtension.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using System;
+using System.Linq;
+
+namespace CodeWF.AvaloniaControls;
+
+public static class TreeDataGridExtension
+{
+    public static void AddSelectAll<T>(this TreeDataGrid treeDataGrid, FlatTreeDataGridSource<T> itemSource) where T : class
+    {
+        treeDataGrid.AddHandler(InputElement.KeyDownEvent, (sender, e) =>
+        {
+            if (e.KeyModifiers == KeyModifiers.Control && e.Key == Key.A)
+            {
+                itemSource.RowSelection?.Clear();
+                itemSource.RowSelection?.BeginBatchUpdate();
+                for(var i = 0; i < itemSource.Rows.Count; i++)
+                {
+                    itemSource.RowSelection?.Select(new IndexPath(i));
+                }
+                itemSource.RowSelection?.EndBatchUpdate();
+                e.Handled = true;
+                itemSource?.RowSelection?.Select(new IndexPath(Enumerable.Range(1, itemSource.Items.Count())));
+            }
+            else if ((e.KeyModifiers == (KeyModifiers.Control | KeyModifiers.Shift)) && e.Key == Key.A)
+            {
+                itemSource?.RowSelection?.Clear();
+                e.Handled = true;
+            }
+        }, Avalonia.Interactivity.RoutingStrategies.Tunnel);
+    }
+}
--- src/TestDataGridDemo/Views/TreeDataGridDynamicDemoView.axaml.cs
+++ src/TestDataGridDemo/Views/TreeDataGridDynamicDemoView.axaml.cs
@@ -1,13 +1,16 @@
 using Avalonia.Controls;
+using CodeWF.AvaloniaControls;
 using TestDataGridDemo.ViewModels;
 
 namespace TestDataGridDemo.Views;
 
 public partial class TreeDataGridDynamicDemoView : UserControl
 {
+    public TreeDataGridDynamicDemoViewModel ViewModel    { get; set; }
     public TreeDataGridDynamicDemoView()
     {
-        DataContext = new TreeDataGridDynamicDemoViewModel();
+        DataContext= ViewModel = new TreeDataGridDynamicDemoViewModel();
         InitializeComponent();
+        MyTree.AddSelectAll(ViewModel.ItemsSource!);
     }
 }
