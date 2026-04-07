e2820ece2621855ad99fef5e081fa894a1f11b3a
Dotnet9(dotnet9.com) <1012434131@qq.com>
2026-04-07 14:04:18 +0800

    Update MainWindow.axaml.cs

--- src/CodeWF.AvaloniaControls.DockReactiveUIDemo/Views/MainWindow.axaml.cs
+++ src/CodeWF.AvaloniaControls.DockReactiveUIDemo/Views/MainWindow.axaml.cs
@@ -24,7 +24,9 @@ public partial class MainWindow : UrsaWindow
     protected override async void OnClosing(WindowClosingEventArgs e)
     {
         e.Cancel = true;
-        await MessageBox.ShowOverlayAsync("Are you sure you want to exit?", "Confirm Exit");
+        // 如果打开了第三方进程的窗口，使用遮罩提示框会被第三方窗口覆盖，导致用户无法看到提示框，因此这里直接使用普通的消息框。
+        //await MessageBox.ShowOverlayAsync("Are you sure you want to exit?", "Confirm Exit");
+        await MessageBox.ShowAsync("Are you sure you want to exit?", "Confirm Exit");
 #if DEBUG
         Environment.Exit(0);
 #endif
